2539d5f1ef55ad521557bdb8ec511adf1ca5c298
Carl Gardiner <carl.gardiner@os.uk>
2023-06-28 12:18:38 +0100

    change hello world to hello gazooks

---
+++ TheGazooks/TheGazooks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheGazooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Gazooks!");
+        }
+    }
+}
