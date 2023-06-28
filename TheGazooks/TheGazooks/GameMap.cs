8d5866b3e8ac1f7fca2692a8f071e6d57cb44729
Carl Gardiner <carl.gardiner@os.uk>
2023-06-28 18:16:44 +0100

    added maplocation classes and therefore have something to work with

--- TheGazooks/TheGazooks/GameMap.cs
+++ TheGazooks/TheGazooks/GameMap.cs
@@ -4,25 +4,62 @@ namespace TheGazooks
 {
     public class GameMap
     {
-        private char[][] _mapArrays;
+        
         private Random r = new Random();
+        private IMapLocation[][] _mapLocations;
         public GameMap(int numOfTiles)
         {
-            char[] potentialArray = new char[] { '0', '&', '4', '!' };
-            _mapArrays = new char[numOfTiles][];
+            _mapLocations = new IMapLocation[numOfTiles][];
             for(int i = 0; i < numOfTiles; ++i)
             {
-                _mapArrays[i] = new char[numOfTiles];
+                _mapLocations[i] = new IMapLocation[numOfTiles];
                 for(int j = 0; j < numOfTiles; ++j)
                 {
+                    // Sets the edge of the map to be mountains, mountains are impassible, therefore stopping the player getting off the map
                     if (i == 0 || j == 0 || i == numOfTiles - 1 || j == numOfTiles - 1)
                     {
-                        _mapArrays[i][j] = '^';
+                        _mapLocations[i][j] = new MountainLocation();
                         continue;
                     }
                     else
                     {
-                        _mapArrays[i][j] = potentialArray[r.Next(4)];
+                        /* There are currently five possible types of location, in order to weight them we'll set them at different probabilities.
+                         * 0,1,2 will be Plains, 3,4,5 will be Forest, 6,7 will be sea, 8 will be city and 9 will be hill
+                         */
+                        int randomNum = r.Next(10);
+                        switch (randomNum)
+                        {
+                            case 0:
+                                _mapLocations[i][j] = new PlainsLocation();
+                                break;
+                            case 1:
+                                _mapLocations[i][j] = new PlainsLocation();
+                                break;
+                            case 2:
+                                _mapLocations[i][j] = new PlainsLocation();
+                                break;
+                            case 3:
+                                _mapLocations[i][j] = new ForestLocation();
+                                break;
+                            case 4:
+                                _mapLocations[i][j] = new ForestLocation();
+                                break;
+                            case 5:
+                                _mapLocations[i][j] = new ForestLocation();
+                                break;
+                            case 6:
+                                _mapLocations[i][j] = new SeaLocation();
+                                break;
+                            case 7:
+                                _mapLocations[i][j] = new SeaLocation();
+                                break;
+                            case 8:
+                                _mapLocations[i][j] = new CityLocation();
+                                break;
+                            case 9:
+                                _mapLocations[i][j] = new HillLocation();
+                                break;
+                        }
                     }
                 }
             }
@@ -32,42 +69,47 @@ namespace TheGazooks
         public void PrintMap()
         {
             Console.BackgroundColor = ConsoleColor.White;
-            for (int i = 0; i < _mapArrays.Length; ++i)
+            for (int i = 0; i < _mapLocations.Length; ++i)
             {
                 
-                for (int j = 0; j < _mapArrays.Length; ++j)
+                for (int j = 0; j < _mapLocations.Length; ++j)
                 {
 
-                    switch (_mapArrays[i][j])
+                    if (_mapLocations[i][j] is PlainsLocation)
                     {
-                        case '0':
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write(_mapArrays[i][j]);
-                            break;
-                        case '&':
-                            Console.ForegroundColor = ConsoleColor.DarkBlue;
-                            Console.Write(_mapArrays[i][j]);
-                            break;
-                        case '4':
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
-                            Console.Write(_mapArrays[i][j]);
-                            break;
-                        case '!':
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.Write(_mapArrays[i][j]);
-                            break;
-                        case '^':
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
-                            Console.Write(_mapArrays[i][j]);
-                            break;
-                        default:
-                            throw new Exception();
-
-
-                            
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(_mapLocations[i][j].Symbol);
                     }
-                    Console.Write(_mapArrays[i][j]);
-                    Console.Write(_mapArrays[i][j]);
+                    else if (_mapLocations[i][j] is SeaLocation)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.Write(_mapLocations[i][j].Symbol);
+                    }
+                    else if(_mapLocations[i][j] is ForestLocation)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write(_mapLocations[i][j].Symbol);
+                    }
+                    else if(_mapLocations[i][j] is HillLocation)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write(_mapLocations[i][j].Symbol);
+                    }
+                    else if(_mapLocations[i][j] is CityLocation)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write(_mapLocations[i][j].Symbol);
+                    }
+                    else if(_mapLocations[i][j] is MountainLocation)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(_mapLocations[i][j].Symbol);
+                    }
+
+                    
+                       
+                    Console.Write(_mapLocations[i][j].Symbol);
+                    Console.Write(_mapLocations[i][j].Symbol);
                     
                     Console.Write(' ');
                 }
---
+++ TheGazooks/TheGazooks/MapLocation.cs
@@ -0,0 +1,86 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace TheGazooks
+{
+    public interface IMapLocation
+    {
+        public char Symbol { get;}
+        public string LocationName { get;}
+        public string LocationDescription { get; }
+    }
+
+    public class ForestLocation : IMapLocation
+    {
+        public char Symbol { get; private set; }
+        public string LocationName { get; private set; }
+        public string LocationDescription { get; private set; }
+
+        public ForestLocation()
+        {
+            Symbol = '?';
+        }
+    }
+
+    public class MountainLocation : IMapLocation
+    {
+        public char Symbol { get; private set; }
+        public string LocationName { get; private set; }
+        public string LocationDescription { get; private set; }
+
+        public MountainLocation()
+        {
+            Symbol = '^';
+        }
+    }
+
+    public class SeaLocation : IMapLocation
+    {
+        public char Symbol { get; private set; }
+        public string LocationName { get; private set; }
+        public string LocationDescription { get; private set; }
+
+        public SeaLocation()
+        {
+            Symbol = '&';
+        }
+    }
+
+    public class PlainsLocation : IMapLocation
+    {
+        public char Symbol { get; private set; }
+        public string LocationName { get; private set; }
+        public string LocationDescription { get; private set; }
+
+        public PlainsLocation()
+        {
+            Symbol = '"';
+        }
+    }
+
+    public class CityLocation : IMapLocation
+    {
+        public char Symbol { get; private set; }
+        public string LocationName { get; private set; }
+        public string LocationDescription { get; private set; }
+
+        public CityLocation()
+        {
+            Symbol = '!';
+        }
+    }
+
+    public class HillLocation : IMapLocation
+    {
+        public char Symbol { get; private set; }
+        public string LocationName { get; private set; }
+        public string LocationDescription { get; private set; }
+
+        public HillLocation()
+        {
+            Symbol = '~';
+        }
+    }
+
+
+
+}
